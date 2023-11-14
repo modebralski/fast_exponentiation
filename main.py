df0631aab4148b04fe57ef5da44c08b4cf22f687
modebralski <m.odebralski@gmail.com>
2023-11-14 20:35:17 +0100

    finished exponentiation()

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 def main():
-    exponentiation(2, 10)
+    print(exponentiation(11, 1000))
 
 
 def exponentiation(base, power):
-    power_in_binary = bin(power)
-    for bit in power_in_binary[2:]:
-        print(bit)
+    power_in_binary = bin(power)[2:]
+    print(f'{power_in_binary}')
+    lenght_of_power_in_binary = len(power_in_binary)
+    squares_of_base = [base]
+    for _ in range(lenght_of_power_in_binary - 1):
+        squares_of_base.append(squares_of_base[-1] * squares_of_base[-1])
+    print(squares_of_base)
+    result = 1
+    for i, current_bit in enumerate(power_in_binary[::-1]):
+        print(f'i {i}, bit {current_bit}')
+        if current_bit == '1':
+            result = result * squares_of_base[i]
+    return result
 
 
 if __name__ == '__main__':
