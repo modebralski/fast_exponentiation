f030387417e32686367628d0f11b7e6401785957
modebralski <m.odebralski@gmail.com>
2023-11-14 19:35:47 +0100

    created exponentiation()

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    exponentiation(2, 10)
+
+
+def exponentiation(base, power):
+    power_in_binary = bin(power)
+    for bit in power_in_binary[2:]:
+        print(bit)
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
